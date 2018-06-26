9a8b0df3b664b7212ffaab36ded2d3725ad2f774
Alfred Vivek <alfredvivekp@gmail.com>
2018-06-26 12:24:09 +0530

    Algorithms
    Backend Data Analysis Algorithms

---
+++ Backend Algorithms/ContentBased.py
@@ -0,0 +1,351 @@
+import collections
+import math
+import operator
+
+import pandas as pd
+import numpy as np
+import random
+import warnings
+from pandas import DataFrame
+import time
+
+movies = pd.read_csv(r"""C:\Users\Smarty\Downloads\untitled\100k\u.item""", sep="|", header=None,encoding = "ISO-8859-1")
+movies.columns = ["movie_id", "movie_title", "release_date", "video_release_date", "IMDb_URL", "unknown", "Action",
+                      "Adventure", "Animation", "Children's", "Comedy", "Crime", "Documentary", "Drama", "Fantasy",
+                      "Film_Noir", "Horror", "Musical", "Mystery", "Romance", "Sci-Fi", "Thriller", "War", "Western"]
+genre = pd.read_csv(r"""C:\Users\Smarty\Downloads\untitled\100k\u.genre""", sep="|", header=None)
+genre.columns=["genre","value"]
+
+
+
+global_int_user_id=0
+# user combination genre : No of movies in a specific genre combinataion
+user_combination_genre={}
+sorted_genre_matrix={}
+final_recommendation_genre_list=[]
+first_filter_movies = pd.DataFrame()
+final_recommendation_movies = pd.DataFrame(
+        columns=["movie_id", "movie_title", "release_date", "video_release_date", "IMDb_URL", "unknown", "Action",
+                 "Adventure", "Animation", "Children's", "Comedy", "Crime", "Documentary", "Drama", "Fantasy",
+                 "Film_Noir", "Horror", "Musical", "Mystery", "Romance", "Sci-Fi", "Thriller", "War", "Western"])
+
+# Creating a list of ratings from a specific user with ratings more than avg. rating
+# Input : userId as int
+# Output : DataFrame consisting of ratings for the input userId with more than the avg rating
+def user_ratings(user_id):
+    df_user_data = user_data.loc[(user_data['user_id'] == int (user_id)) & (user_data['rating']>=3)]
+    # print("USER RATINGS - Total number of ratings under consideration :"+str(len(df_user_data)))
+    return df_user_data
+
+# Creatng each movie rating according to genre and finding the user's combinations in genre
+def create_genre_combination(df):
+    gener_matrix = []
+    k=[]
+    for i in range(len(df.index)):
+        movie_df = movies.loc[movies["movie_id"] == df.iat[i, 1]]
+        temp_ls = []
+        temp_combi_genre=[]
+        for j in range(5, len(movies.columns)):
+            temp_ls.append((movie_df.iat[0, j])*(df.iat[i, 2]))
+            if(movie_df.iat[0, j]==1):
+                temp_combi_genre.append(genre.iat[j-5,0])
+        gener_matrix.append(temp_ls)
+        if (tuple(temp_combi_genre) not in user_combination_genre.keys()):
+            user_combination_genre[tuple(temp_combi_genre)] = 1
+        else:
+            user_combination_genre[tuple(temp_combi_genre)]+=1
+    # print(user_combination_genre)
+    return gener_matrix
+
+# Ratings of Each Genre depending on the user rating
+def get_average_genre_ratings(d):
+    df = DataFrame(d)
+    list_gener_matrix =create_genre_combination(df)
+    gener_main=[]
+    for i in range (len(list_gener_matrix[0])):
+        sum=0
+        for j in range (len(list_gener_matrix)):
+            sum +=list_gener_matrix[j][i]
+        gener_main.append((int)((sum/len(list_gener_matrix)*5)))
+    # print (gener_main)
+    return gener_main
+
+# Top Genre dict
+def top_genere_values(gener_values):
+
+    avg=sum(gener_values)/len(gener_values)
+    for i in range(len(gener_values)):
+        if(gener_values[i]>=avg):
+            sorted_genre_matrix[genre.iat[i,0]]=gener_values[i]
+    # print("Genres Watched Regularly : "+str(sorted_genre_matrix))
+
+#calculate recommended genre combinations
+def find_accurate_recomendation_genre(combination_genres_sorted_list):
+    sum=0
+    items = list(combination_genres_sorted_list.items())
+    for i in range(0,len(items)):
+        sum+=items[i][1]
+    avg = math.ceil (sum/len(combination_genres_sorted_list))
+    for i in range(0,len(items)):
+        if(items[i][1]>=avg):
+            final_recommendation_genre_list.append(items[i][0])
+    # print ("Combination of genres that can be recommended :"+ str (final_recommendation_genre_list))
+
+#Calculating recommended Genre Lists
+def calculate_recommended_genres():
+    temp_dict=user_combination_genre
+    for i in temp_dict.keys():
+        sum=0
+        for j in range(0,len(i)):
+            if(i[j] in sorted_genre_matrix):
+                sum+=sorted_genre_matrix[i[j]]*user_combination_genre[i]
+        temp_dict[i]=sum
+    temp_dict=sorted(temp_dict.items(),key=lambda x : x[1],reverse=True)
+    combination_genres_sorted_list = collections.OrderedDict(temp_dict)
+    find_accurate_recomendation_genre(combination_genres_sorted_list)
+
+#input : nothing, output : movie dict , all movies with the sorted_genre_matrix
+def filter_movies_according_to_most_viewed_genre():
+    temp_m1 = pd.DataFrame(
+        columns=["movie_id", "movie_title", "release_date", "video_release_date", "IMDb_URL", "unknown", "Action",
+                 "Adventure", "Animation", "Children's", "Comedy", "Crime", "Documentary", "Drama", "Fantasy",
+                 "Film_Noir", "Horror", "Musical", "Mystery", "Romance", "Sci-Fi", "Thriller", "War", "Western"])
+
+    list_keys = list(sorted_genre_matrix.keys())
+    for i in range(0, len(list_keys)):
+        temp = DataFrame(movies.loc[movies[list_keys[i]] == 1])
+        frames = [temp_m1, temp]
+        temp_m1 = pd.concat(frames)
+    temp_m1 = temp_m1.drop_duplicates(subset=['movie_id'])
+    global first_filter_movies
+    first_filter_movies = temp_m1
+    # print("Number of Movies that can be recommended: %d" % (len(first_filter_movies)))
+
+#input : nothing, output : movie dict , all movies which are to be recommended to the user
+def secondary_movie_filtering():
+    for i in range(0,len(final_recommendation_genre_list)):
+        temp_m1 = pd.DataFrame(
+            columns=["movie_id", "movie_title", "release_date", "video_release_date", "IMDb_URL", "unknown", "Action",
+                     "Adventure", "Animation", "Children's", "Comedy", "Crime", "Documentary", "Drama", "Fantasy",
+                     "Film_Noir", "Horror", "Musical", "Mystery", "Romance", "Sci-Fi", "Thriller", "War", "Western"])
+        for j in range(0,len(final_recommendation_genre_list[i])):
+            temp = DataFrame(first_filter_movies.loc[first_filter_movies[final_recommendation_genre_list[i][j]]==1])
+            if(len(temp)==0):
+                continue
+            else:
+                frames=[temp_m1,temp]
+                temp_m1=pd.concat(frames)
+        temp_m1 = temp_m1.drop_duplicates(subset=['movie_id'])
+        if(len(temp)!=0):
+            recommending_movies(temp)
+
+def print_full_df(x):
+    with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+        print(x)
+
+#input : movie id, output : boolean value based on whether the movie has been watched or not
+def check_if_already_saw(id):
+    return (len(user_data.loc[(user_data["movie_id"]==id) &(user_data["user_id"]==int (global_int_user_id))] ))!=0
+
+#input : dataframe, output : boolean value based on whether the movie has been added to the final list or not
+def alredy_added_movie(temp):
+    return not ((len(final_recommendation_movies.loc[final_recommendation_movies["movie_id"]==temp.iloc[0,0]])))!=0
+
+#input : dataframe, output : movie dict , final movie recomendation list
+def add_movie_to_dataframe(temp):
+    global  final_recommendation_movies
+    if len(final_recommendation_movies)==0:
+        frames=[final_recommendation_movies,temp]
+        final_recommendation_movies=pd.concat(frames)
+        return True
+    else:
+        if(alredy_added_movie(temp)):
+            frames = [final_recommendation_movies, temp]
+            final_recommendation_movies = pd.concat(frames)
+        return True
+    return False
+
+#input : dataframe, output : picking up random movies from each genre and preparing a list of 5 movies to be recommended to the user.
+def recommending_movies(x):
+    while_counter=0
+    movie_counter=0
+    while(True):
+        if(while_counter==len(x) or movie_counter==20):
+            return
+        rand = random.randint(0,len(x)-1)
+        temp=(x.iloc[rand,0])
+        temp=movies.loc[(movies['movie_id']==temp)]
+        if(not check_if_already_saw(temp.iloc[0,0])):
+            flag=add_movie_to_dataframe(temp)
+            if(flag):
+                movie_counter+=1
+        while_counter+=1
+
+#display the final list of recommended movies to the user
+# def show_movie_names():
+#     print("RECOMMENDED MOVIES")
+    # if(len(final_recommendation_movies)<10):
+    #     for i in range(0,len(final_recommendation_movies)):
+    #         print (final_recommendation_movies.iloc[i,1])
+    # else:
+    #     for i in range(0,10):
+    #         print(final_recommendation_movies.iloc[i,1])
+    # print(final_recommendation_movies)
+
+#main method invoking all the other user defined functions
+
+
+class cool:
+    def collabrative(self):
+        time_start = time.time()
+        global user_data
+        mean = user_data.groupby(['user_id'],as_index=False,sort=False).mean().rename(columns={'rating':'rating_mean'})[['user_id','rating_mean']]
+        user_data.groupby(['user_id'], as_index=False, sort=False).mean().rename(columns={'rating': 'rating_mean'})[
+            ['user_id', 'rating_mean']]
+        user_data = pd.merge(user_data, mean, on='user_id', how='left', sort=False)
+        user_data['rating_adjusted'] = user_data['rating'] - user_data['rating_mean']
+        distinct_users = np.unique(user_data['user_id'])
+        user_data_append = pd.DataFrame()
+        user_data_all = pd.DataFrame()
+
+        # print (CB.global_int_user_id)
+        user1_data = user_data[(user_data['user_id'] == int(global_int_user_id))]
+        user1_mean = user1_data['rating'].mean()
+        user1_data = user1_data.rename(columns={'user_id': 'user_id1'})
+        user1_data = user1_data.rename(columns={'rating_adjusted': 'rating_adjusted1'})
+        user1_val = np.sqrt(np.sum(np.square(user1_data['rating_adjusted1']), axis=0))
+        # print(user1_data)
+        i = 1
+        distinct_movie = np.unique(user_data['movie_id'])
+
+        for movie in distinct_movie:
+            item_user = user_data[(user_data['movie_id'] == int(movie))]
+            distinct_users1 = np.unique(item_user['user_id'])
+            j = 1
+            for user2 in distinct_users1:
+                # if j% 200 == 0:
+                #     print (j,"out of",len(distinct_users1), i , "out of", len(distinct_movie))
+                user2_data = user_data[(user_data['user_id'] == user2)]
+                # user2_mean = user2_data['rating'].mean()
+                user2_data = user2_data.rename(columns={'user_id': 'user_id2'})
+                user2_data = user2_data.rename(columns={'rating_adjusted': 'rating_adjusted2'})
+                user2_val = np.sqrt(np.sum(np.square(user2_data['rating_adjusted2']), axis=0))
+                user_data_1 = pd.merge(user1_data, user2_data[['rating_adjusted2', 'movie_id', 'user_id2']],
+                                        on='movie_id', how='inner', sort=False)
+                user_data_1['vector_product'] = (user_data_1['rating_adjusted1'] * user_data_1['rating_adjusted2'])
+                user_data_1 = user_data_1.groupby(['user_id1', 'user_id2'], as_index=False, sort=False).sum()
+                user_data_1['dot'] = user_data_1['vector_product'] / (user1_val * user2_val)
+                user_data_all = user_data_all.append(user_data_1, ignore_index=True)
+                j = j + 1
+            # print (len(user_data_all),"---------------------------------------")
+            user_data_all = user_data_all[(user_data_all['dot'] < 1) & (user_data_all['dot'] > (-1))]
+            user_data_all = user_data_all.sort_values(['dot'], ascending=False)
+            user_data_all = user_data_all.head(50)
+            # print(user_data_all)
+            # print("------------------------------------------")
+            user_data_all['movie_id'] = movie
+            user_data_append = user_data_append.append(user_data_all, ignore_index=True)
+            # print(user_data_all)
+            # print (len(user_data_all),"+++++++++++++++++++++++++++++++++++++++")
+            i = i + 1
+        User_dot_adj_rating_all = pd.DataFrame()
+        distinct_movie = np.unique(user_data['movie_id'])
+        j = 1
+        for movie in distinct_movie:
+            user_data_append_movie = user_data_append[user_data_append['movie_id'] == movie]
+            User_dot_adj_rating = pd.merge(user_data, user_data_append_movie[['dot', 'user_id2', 'user_id1']],
+                                              how='inner', left_on='user_id', right_on='user_id2', sort='False')
+            # if j % 200 == 0 :
+            #     print (j,"out of" , len(distinct_movie))
+            User_dot_adj_rating1 = User_dot_adj_rating[User_dot_adj_rating['movie_id'] == movie]
+            if len(np.unique(User_dot_adj_rating1['user_id'])) >= 1:
+                try:
+                    User_dot_adj_rating1['rating'] = User_dot_adj_rating1['dot'] * User_dot_adj_rating1[
+                        'rating_adjusted']
+                    User_dot_adj_rating1['dot_abs'] = User_dot_adj_rating1['dot'].abs()
+                except warnings:
+                    print()
+
+                User_dot_adj_rating1 = User_dot_adj_rating1.groupby(['user_id1'], as_index=False, sort=False).sum()[
+                    ['user_id1', 'rating', 'dot_abs']]
+                User_dot_adj_rating1['Rating'] = int(
+                    (User_dot_adj_rating1['rating'] / User_dot_adj_rating1['dot_abs']) + user1_mean)
+                User_dot_adj_rating1['movie_id'] = movie
+                User_dot_adj_rating1 = User_dot_adj_rating1.drop(['rating', 'dot_abs'], axis=1)
+                User_dot_adj_rating_all = User_dot_adj_rating_all.append(User_dot_adj_rating1, ignore_index=True)
+            j = j + 1
+        User_dot_adj_rating_all = User_dot_adj_rating_all.sort_values(['Rating'], ascending=False)
+        # print ( '+++++++++++++++++++++++++++++',User_dot_adj_rating_all)
+
+        collabrative_movies = {}
+        for row in User_dot_adj_rating_all.iterrows():
+            # print (row[1][2])
+            collabrative_movies[(movies.iloc[row[1][2]][1])] = (movie_count.loc[row[1][2]][0])
+        # print('--------------------------',collabrative_movies)
+        collabrative_movies = sorted(collabrative_movies.items(), key=operator.itemgetter(1))
+        print(collabrative_movies)
+
+        # for row in User_dot_adj_rating_all:
+
+        s1 = pd.merge(final_recommendation_movies, User_dot_adj_rating_all, how='inner', on=['movie_id'])
+        s1 = pd.DataFrame(s1.loc[s1['Rating'] >= 3])
+
+        # print (s1)
+        mixed = {}
+        for row in s1.iterrows():
+            # print ('_+++++++++++++++++++',row[1][0])
+            mixed[(movies.iloc[row[1][0]][1])] = (movie_count.loc[row[1][0]][0])
+        mixed = sorted(mixed.items(), key=operator.itemgetter(1))
+        print(mixed)
+
+        # for i in range(len(s1)):
+        #     print (s1.iloc[i,1])
+        # print (user_data_append)
+        time_end = time.time()
+        # print("Execution Time :" + str(time_end - time_start))
+
+        # print(user_data_all)
+
+        # print(CB.user_data)
+def main():
+
+    global global_int_user_id,user_data,movie_count
+    # global_int_user_id = input("Enter User ID :")
+    for i in range(801,900):
+        user_data = pd.read_csv(r"""C:\Users\Smarty\Downloads\untitled\100k\u.data""", sep="\t", header=None)
+        user_data.columns = ["user_id", "movie_id", "rating", "ts"]
+        movie_count = DataFrame(user_data.groupby(['movie_id']).size())
+
+        global_int_user_id=i
+        time_start = time.time()
+        df_user_data=user_ratings(global_int_user_id)
+        list_avg_gener_values=get_average_genre_ratings(df_user_data)
+        top_genere_values(list_avg_gener_values)
+        calculate_recommended_genres()
+        filter_movies_according_to_most_viewed_genre()
+        secondary_movie_filtering()
+        # show_movie_names()
+        # print (pd.merge(movies, movie_count, how='outer',on='movie_id'))
+        # print ("Total number of movies that can be recommended from contentbased :"+ str(len(final_recommendation_movies)))
+
+        content_movies={}
+        # print (final_recommendation_movies)
+        for row in final_recommendation_movies.iterrows():
+            # print((movies.iloc[row[0]][1]))
+            # print (type(movie_count.loc[row[0]][0]))
+            content_movies[(movies.iloc[row[0]][1])]=([movie_count.loc[row[0]][0]])
+            # content_movies[row.loc[0,2]]
+        # print('-------------------------------------------------------------------------------------------------------------')
+        print ('User Id = ',global_int_user_id)
+        # print (content_movies)
+        time_end = time.time()
+        content_movies = sorted(content_movies.items(), key=lambda e: e[1][0])
+        print(content_movies)
+        a = cool()
+        a.collabrative()
+
+
+    # print ("Execution Time :"+ str(time_end-time_start))
+
+
+main()
